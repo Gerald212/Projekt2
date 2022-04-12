e030698af7053883a52b5d3627edad2882f4607b
Student <huntergerald00@gmail.com>
2022-04-12 16:12:39 +0200

    commit 4

--- app/src/main/java/com/example/bazydanych/MainActivity.java
+++ app/src/main/java/com/example/bazydanych/MainActivity.java
@@ -2,15 +2,22 @@ package com.example.bazydanych;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText NameInput, DescriptionInput;
-    TextView DataView;
+    TextView ResultTextView;
+    private Snackbar mySnackbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,14 +26,39 @@ public class MainActivity extends AppCompatActivity {
 
         NameInput = (EditText) findViewById(R.id.editTextTextName);
         DescriptionInput = (EditText) findViewById(R.id.editTextTextDescription);
-        DataView = (TextView) findViewById(R.id.textViewData);
-    }
-
-    public void butonInsertDataOnClick(View view){
+        ResultTextView = (TextView) findViewById(R.id.textViewData);
 
     }
 
-    public void butonDisplayDataOnClick(View view){
+    public void buttonInsertDataOnClick(View view){
+        ContentValues values = new ContentValues();
 
+        // fetching text from user
+        values.put(MyContentProvider.name, (NameInput.getText().toString()));
+        values.put(MyContentProvider.description, (DescriptionInput.getText().toString()));
+
+        getContentResolver().insert(MyContentProvider.CONTENT_URI, values);
+
+        mySnackbar = Snackbar.make(view, "Dodano użytkownika", 2000);
+        mySnackbar.show();
+    }
+
+    public void buttonDisplayDataOnClick(View view){
+        
+        Cursor cursor = getContentResolver().query(Uri.parse("content://com.demo.user.provider/users"), null, null, null, null);
+
+        // iteration of the cursor
+        // to print whole table
+        if(cursor.moveToFirst()) {
+            StringBuilder strBuild=new StringBuilder();
+            while (!cursor.isAfterLast()) {
+                strBuild.append("\n" + cursor.getString(cursor.getColumnIndex("id")) + "-" + cursor.getString(cursor.getColumnIndex("name")));
+                cursor.moveToNext();
+            }
+            ResultTextView.setText(strBuild);
+        }
+        else {
+            ResultTextView.setText("No Records Found");
+        }
     }
 }
--- app/src/main/java/com/example/bazydanych/MyContentProvider.java
+++ app/src/main/java/com/example/bazydanych/MyContentProvider.java
@@ -1,10 +1,15 @@
 package com.example.bazydanych;
 
 import android.content.ContentProvider;
+import android.content.ContentUris;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.UriMatcher;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteQueryBuilder;
 import android.net.Uri;
 
 import java.util.HashMap;
@@ -22,6 +27,7 @@ public class MyContentProvider extends ContentProvider {
 
     static final String id = "id";
     static final String name = "name";
+    static final String description = "description";
     static final int uriCode = 1;
     static final UriMatcher uriMatcher;
     private static HashMap<String, String> values;
@@ -56,16 +62,72 @@ public class MyContentProvider extends ContentProvider {
         return false;
     }
 
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+        SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
+        qb.setTables(TABLE_NAME);
+        switch (uriMatcher.match(uri)) {
+            case uriCode:
+                qb.setProjectionMap(values);
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown URI " + uri);
+        }
+        if (sortOrder == null || sortOrder == "") {
+            sortOrder = id;
+        }
+        Cursor c = qb.query(db, projection, selection, selectionArgs, null,
+                null, sortOrder);
+        c.setNotificationUri(getContext().getContentResolver(), uri);
+        return c;
+    }
+
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        long rowID = db.insert(TABLE_NAME, "", values);
+        if (rowID > 0) {
+            Uri _uri = ContentUris.withAppendedId(CONTENT_URI, rowID);
+            getContext().getContentResolver().notifyChange(_uri, null);
+            return _uri;
+        }
+        throw new SQLiteException("Failed to add a record into " + uri);
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+        int count = 0;
+        switch (uriMatcher.match(uri)) {
+            case uriCode:
+                count = db.update(TABLE_NAME, values, selection, selectionArgs);
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown URI " + uri);
+        }
+        getContext().getContentResolver().notifyChange(uri, null);
+        return count;
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        int count = 0;
+        switch (uriMatcher.match(uri)) {
+            case uriCode:
+                count = db.delete(TABLE_NAME, selection, selectionArgs);
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown URI " + uri);
+        }
+        getContext().getContentResolver().notifyChange(uri, null);
+        return count;
+    }
 
 
+
+    // tworzenie bazy danych
     private SQLiteDatabase db;
-
     static final String DATABASE_NAME = "UsersDataBase";
-
     static final String TABLE_NAME = "Users";
-
     static final int DATABASE_VERSION = 1;
-
     static final String CREATE_DB_TABLE = (
             " CREATE TABLE " +
                     TABLE_NAME +
