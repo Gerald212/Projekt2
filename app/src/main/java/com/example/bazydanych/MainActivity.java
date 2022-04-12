1c7fea514c81e0e9a4520b58f55a7ce4ec5ce0bf
Student <huntergerald00@gmail.com>
2022-04-12 15:38:02 +0200

    dommit 2

--- app/src/main/java/com/example/bazydanych/MainActivity.java
+++ app/src/main/java/com/example/bazydanych/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.bazydanych;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText NameInput, DescriptionInput;
+    TextView DataView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        NameInput = (EditText) findViewById(R.id.editTextTextName);
+        DescriptionInput = (EditText) findViewById(R.id.editTextTextDescription);
+        DataView = (TextView) findViewById(R.id.textViewData);
+    }
+
+    public void butonInsertDataOnClick(View view){
+
+    }
+
+    public void butonDisplayDataOnClick(View view){
+
     }
 }
---
+++ app/src/main/java/com/example/bazydanych/MyContentProvider.java
@@ -0,0 +1,7 @@
+package com.example.bazydanych;
+
+import android.content.ContentProvider;
+
+
+public class MyContentProvider extends ContentProvier{
+}
