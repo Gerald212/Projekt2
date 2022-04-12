b8b3f38678f8842872926f38bd9df0292eabbb08
Student <huntergerald00@gmail.com>
2022-04-12 15:53:46 +0200

    commit 3

--- app/src/main/java/com/example/bazydanych/MyContentProvider.java
+++ app/src/main/java/com/example/bazydanych/MyContentProvider.java
@@ -1,7 +1,95 @@
 package com.example.bazydanych;
 
 import android.content.ContentProvider;
+import android.content.Context;
+import android.content.UriMatcher;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.net.Uri;
+
+import java.util.HashMap;
 
 
-public class MyContentProvider extends ContentProvier{
+public class MyContentProvider extends ContentProvider {
+    public MyContentProvider() {
+    }
+
+    static final String PROVIDER_NAME = "com.demo.user.provider";
+
+    static final String URL = "content://" + PROVIDER_NAME + "/users";
+
+    static final Uri CONTENT_URI = Uri.parse(URL);
+
+    static final String id = "id";
+    static final String name = "name";
+    static final int uriCode = 1;
+    static final UriMatcher uriMatcher;
+    private static HashMap<String, String> values;
+
+    static {
+        uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+        uriMatcher.addURI(PROVIDER_NAME, "users", uriCode);
+
+        uriMatcher.addURI(PROVIDER_NAME, "users/*", uriCode);
+    }
+
+    @Override
+    public String getType(Uri uri) {
+        switch (uriMatcher.match(uri)) {
+            case uriCode:
+                return "vnd.android.cursor.dir/users";
+            default:
+                throw new IllegalArgumentException("Unsupported URI: " + uri);
+        }
+    }
+
+
+    @Override
+    public boolean onCreate() {
+        Context context = getContext();
+        DatabaseHelper dbHelper = new DatabaseHelper(context);
+        db = dbHelper.getWritableDatabase();
+        if (db != null) {
+            return true;
+        }
+        return false;
+    }
+
+
+
+    private SQLiteDatabase db;
+
+    static final String DATABASE_NAME = "UsersDataBase";
+
+    static final String TABLE_NAME = "Users";
+
+    static final int DATABASE_VERSION = 1;
+
+    static final String CREATE_DB_TABLE = (
+            " CREATE TABLE " +
+                    TABLE_NAME +
+                    " (id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    " name TEXT NOT NULL, " +
+                    " description TEXT);"
+    );
+
+    private static class DatabaseHelper extends SQLiteOpenHelper {
+
+        DatabaseHelper(Context context) {
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL(CREATE_DB_TABLE);
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+            onCreate(db);
+        }
+    }
+
 }
